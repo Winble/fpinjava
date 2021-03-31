e01a1313d75691f2dfef0633d1ce057d55f9a793
bowenzhang <bowenzhang@trip.com>
2021-03-31 14:42:10 +0800

    add IFunction

---
+++ fpinjava-in-action/src/main/java/introduction/IFunction.java
@@ -0,0 +1,19 @@
+package introduction;
+
+/**
+ * Create on 2021/3/31
+ *
+ * @author bowenzhang
+ */
+public interface IFunction<T, R> {
+
+    R apply(T arg);
+
+    default <V> IFunction<V, R> compose(IFunction<V, T> f) {
+        return x -> apply(f.apply(x));
+    }
+    
+    static <T, R, V> IFunction<V, R> compose(IFunction<T, R> f, IFunction<V, T> g) {
+        return x -> f.apply(g.apply(x));
+    }
+}
